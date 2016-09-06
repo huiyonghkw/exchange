2b195efe8aebb2280239b73c55862e7b9b3f61f1
程会勇 <chenghuiyong@chehubao.com>
2016-09-06 16:42:51 +0800

    namespace update

--- src/ExchangeProvider.php
+++ src/ExchangeProvider.php
@@ -36,7 +36,7 @@ class ExchangeProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton(['Bravist\\Exchange' => 'Exchange'], function($app){
+        $this->app->singleton(['Bravist\\Exchange\\Authorize' => 'Exchange'], function($app){
             return new Authorize(app('rsa'), app('guzzle.http.client'), config('exchange'));
         });
     }
